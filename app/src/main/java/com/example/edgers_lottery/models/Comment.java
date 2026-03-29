7faf2d9ae5a8cc56b0c503363dc493fbb77c3871
siwira <siwira@ualberta.ca>
2026-03-28 22:32:29 -0600

    create comments class

---
+++ app/src/main/java/com/example/edgers_lottery/models/Comment.java
@@ -0,0 +1,41 @@
+package com.example.edgers_lottery.models;
+
+public class Comment {
+    // Fields for the comment
+    private String userID;
+    private String eventID;
+    private String commentText;
+    private String timestamp;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
